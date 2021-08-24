20f1898b77340bae27d298468bb32ba949be39da
unknown <ehssamimi@gmail.com>
2021-08-24 09:42:43 +0430

    add first branch

---
+++ src/Common/ReduxFolder/ReduxTest.js
@@ -0,0 +1,15 @@
+import React  from 'react';
+
+const ReduxTest = (props) => {
+
+
+    return (
+        <div>
+            <h1 className='text-center  '>redux</h1>
+
+
+        </div>
+    );
+};
+
+export default ReduxTest;
